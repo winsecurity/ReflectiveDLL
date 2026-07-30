4867c26edc382881bb7cfd7744ddfd463c278af5
nikhil-polymath <nagasainikhil100@gmail.com>
2026-07-30 17:59:29 +0530

    basic reflective dll loader

---
+++ dllmain.cpp
@@ -0,0 +1,272 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include <Windows.h>
+#include <iostream>
+
+
+
+BOOL __declspec(dllexport) DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBoxA(NULL, "test", "titletest", 0);
+		break;
+
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		//MessageBoxA(NULL, "test", "test", 0);
+		break;
+	}
+	return TRUE;
+}
+
+
+
+extern "C" void __declspec(dllexport) test() {
+
+	 ULONGLONG loadlibraryaddress = 0;
+	 ULONGLONG getprocaddressaddress = 0;
+	 ULONGLONG virtualallocaddress = 0;
+	 ULONGLONG ourdllbase = 0;
+	 auto ppeb = __readgsqword(0x60);
+	 // ldr address
+	 ULONGLONG ldr = *(ULONGLONG*)((char*)ppeb + 0x18);
+	 // ldr+0x10 inloadordermodulelist
+	 ULONGLONG firstldrdatatableentry = *(ULONGLONG*)((char*)ldr + 0x10);
+
+	 while (firstldrdatatableentry != ldr + 0x10) {
+		 // ldrdatatableentry+0x58 basedllname UNICODE_STRING, 
+		 ULONGLONG namebuf = *(ULONGLONG*)((char*)firstldrdatatableentry + 0x58 + 0x8);
+		 //std::wcout << std::hex << "dllname: " << (wchar_t*)namebuf << std::endl;
+
+		 char* namebufptr = (char*)namebuf;
+		 if (*namebufptr == 'K' && (*(namebufptr + 2)) == 'E'
+			 && (*(namebufptr + 4)) == 'R'
+			 && (*(namebufptr + 6)) == 'N' && (*(namebufptr + 8)) == 'E'
+			 && (*(namebufptr + 10)) == 'L' && (*(namebufptr + 12)) == '3'
+			 && (*(namebufptr + 14)) == '2') {
+	
+			ULONGLONG dllbase = *(ULONGLONG*)(firstldrdatatableentry + 0x30);
+			 
+			 IMAGE_DOS_HEADER* dosheader = (IMAGE_DOS_HEADER*)dllbase;
+			 IMAGE_FILE_HEADER* fileheader = (IMAGE_FILE_HEADER*)((char*)dllbase + dosheader->e_lfanew + 4);
+			 IMAGE_OPTIONAL_HEADER64* optionalheader = (IMAGE_OPTIONAL_HEADER64*)((char*)dllbase + dosheader->e_lfanew + 4 + sizeof(IMAGE_FILE_HEADER));
+			 IMAGE_EXPORT_DIRECTORY* exportdirectory = (IMAGE_EXPORT_DIRECTORY*)((char*)dllbase + optionalheader->DataDirectory[0].VirtualAddress);
+
+			
+			 auto entptr = (char*)dllbase + exportdirectory->AddressOfNames;
+			 auto eotptr = (char*)dllbase + exportdirectory->AddressOfNameOrdinals;
+			 auto eatptr = (char*)dllbase + exportdirectory->AddressOfFunctions;
+
+			 for (int i = 0;i < exportdirectory->NumberOfNames;i++) {
+				 ULONG namerva = *(ULONG*)((char*)entptr + i * 4);
+				 SHORT ordinal = *(SHORT*)((char*)eotptr + i * 2);
+				 ULONG funcrva = *(ULONG*)((char*)eatptr + ordinal * 4);
+
+				 ULONGLONG funcaddress = dllbase + funcrva;
+				 char* funcnameptr = (char*)dllbase + namerva;
+				 if (funcnameptr[0] == 'V' && funcnameptr[1] == 'i' &&
+					 funcnameptr[2] == 'r' && funcnameptr[3] == 't' &&
+					 funcnameptr[4] == 'u' && funcnameptr[5] == 'a' && funcnameptr[6] == 'l' &&
+					 funcnameptr[7] == 'A' && funcnameptr[8] == 'l' && funcnameptr[9] == 'l' &&
+					 funcnameptr[10] == 'o' && funcnameptr[11] == 'c' && funcnameptr[12] == '\0') {
+
+			
+					 virtualallocaddress = funcaddress;
+				 }
+
+
+				 if (funcnameptr[0] == 'G' && funcnameptr[1] == 'e' &&
+					 funcnameptr[2] == 't' && funcnameptr[3] == 'P' &&
+					 funcnameptr[4] == 'r' && funcnameptr[5] == 'o' && funcnameptr[6] == 'c' &&
+					 funcnameptr[7] == 'A' && funcnameptr[8] == 'd' && funcnameptr[9] == 'd' &&
+					 funcnameptr[10] == 'r' && funcnameptr[11] == 'e' && funcnameptr[12] == 's') {
+
+				
+					 getprocaddressaddress = funcaddress;
+				 }
+
+
+
+				 if (funcnameptr[0] == 'L' && funcnameptr[1] == 'o' &&
+					 funcnameptr[2] == 'a' && funcnameptr[3] == 'd' &&
+					 funcnameptr[4] == 'L' && funcnameptr[5] == 'i' && funcnameptr[6] == 'b' &&
+					 funcnameptr[7] == 'r' && funcnameptr[8] == 'a' && funcnameptr[9] == 'r' &&
+					 funcnameptr[10] == 'y' && funcnameptr[11] == 'A') {
+
+					
+					 loadlibraryaddress = funcaddress;
+				 }
+
+
+				 //std::cout << "functionname: " << (char*)dllbase + namerva << std::endl;
+
+				 //std::cout << "Funcaddress: " << funcaddress << std::endl;
+
+			 }
+
+		 }
+
+
+
+		 firstldrdatatableentry = *(ULONGLONG*)(firstldrdatatableentry);
+
+	 }
+
+
+	 auto mainaddr = (char*)&DllMain;
+	 while (true) {
+
+		 if (*mainaddr == 'M' && *(mainaddr + 1) == 'Z') {
+			 break;
+		 }
+
+		 mainaddr--;
+	 }
+
+	 ourdllbase = (ULONGLONG)(mainaddr);
+
+	 
+	 IMAGE_DOS_HEADER* dosheader = (IMAGE_DOS_HEADER*)ourdllbase;
+	 IMAGE_FILE_HEADER* fileheader = (IMAGE_FILE_HEADER*)((char*)ourdllbase + dosheader->e_lfanew + 4);
+	 IMAGE_OPTIONAL_HEADER64* optionalheader = (IMAGE_OPTIONAL_HEADER64*)((char*)ourdllbase +
+		 dosheader->e_lfanew + 4 + sizeof(IMAGE_FILE_HEADER));
+
+	 auto virtualallocaddressrunner = (LPVOID(*)(LPVOID, SIZE_T, DWORD, DWORD)) virtualallocaddress;
+	 LPVOID baseaddress = virtualallocaddressrunner(NULL, optionalheader->SizeOfImage, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+
+	 // copying headers
+	 for (int i = 0;i < optionalheader->SizeOfHeaders;i++) {
+		 *((char*)baseaddress + i) = *((char*)ourdllbase + i);
+	 }
+
+
+
+
+	 // copying sections to their respective virutal addresses
+	 IMAGE_SECTION_HEADER* sectionheader = (IMAGE_SECTION_HEADER*)
+		 ((char*)ourdllbase +
+			 dosheader->e_lfanew + 4 +
+			 sizeof(IMAGE_FILE_HEADER) + fileheader->SizeOfOptionalHeader);
+
+
+
+	 for (int i = 0;i < fileheader->NumberOfSections;i++) {
+
+
+		 auto sectionva = sectionheader->VirtualAddress;
+		 auto rawdata = sectionheader->PointerToRawData;
+		 auto sizeofrawdata = sectionheader->SizeOfRawData;
+		 for (int j = 0;j < sizeofrawdata;j++) {
+
+			 *(char*)((char*)baseaddress + sectionva + j) =
+				 *(char*)((char*)ourdllbase + rawdata + j);
+		 }
+
+
+		 sectionheader++;
+	 }
+
+
+	 //dosheader = (IMAGE_DOS_HEADER*)baseaddress;
+	 // fileheader = (IMAGE_FILE_HEADER*)((char*)baseaddress + dosheader->e_lfanew + 4);
+	 //optionalheader = (IMAGE_OPTIONAL_HEADER64*)((char*)baseaddress +
+	 //	dosheader->e_lfanew + 4 + sizeof(IMAGE_FILE_HEADER));
+
+	 // fixing base relocations
+	 if (optionalheader->DataDirectory[5].Size) {
+
+		 IMAGE_BASE_RELOCATION* baserelocation = (IMAGE_BASE_RELOCATION*)((char*)baseaddress + optionalheader->DataDirectory[5].VirtualAddress);
+
+		 auto delta = (LONGLONG)baseaddress - optionalheader->ImageBase;
+
+		 while (baserelocation->VirtualAddress) {
+
+			 auto pagerva = (char*)baseaddress + baserelocation->VirtualAddress;
+			 auto blocksize = baserelocation->SizeOfBlock;
+
+
+			 for (int i = 0;i < (blocksize - 8) / 2;i++) {
+
+				 WORD* entry = (WORD*)((char*)baserelocation + 8 + i * 2);
+				 WORD entryvalue = (*entry) & 0x0FFF;
+				 auto relocrva = pagerva + entryvalue;
+
+				 ULONGLONG oldaddress = *(ULONGLONG*)relocrva;
+				 ULONGLONG newaddress = oldaddress + delta;
+
+				 // writing new address
+				 *(ULONGLONG*)relocrva = newaddress;
+
+
+			 }
+
+			 baserelocation = (IMAGE_BASE_RELOCATION*)((char*)baserelocation + blocksize);
+
+		 }
+
+
+
+
+
+	 }
+
+
+	 // fixing imports
+	 if (optionalheader->DataDirectory[1].Size) {
+
+		 IMAGE_IMPORT_DESCRIPTOR* importptr = (IMAGE_IMPORT_DESCRIPTOR*)((char*)baseaddress + 
+			 optionalheader->DataDirectory[1].VirtualAddress);
+
+		 while (importptr->Name) {
+
+			 auto dllnameptr = (char*)baseaddress + importptr->Name;
+
+			 HMODULE dllhandle = ((HMODULE(*)(LPCSTR))loadlibraryaddress) (dllnameptr);
+
+			 ULONGLONG* ogfirstthunkptr = (ULONGLONG*)((char*)baseaddress + importptr->OriginalFirstThunk);
+			 ULONGLONG* firstthunkptr = (ULONGLONG*)((char*)baseaddress + importptr->FirstThunk);
+
+			 while (*ogfirstthunkptr) {
+
+
+				 char* funcnamerva = (char*)baseaddress + *ogfirstthunkptr + 2;
+
+				 FARPROC funcaddress = ((FARPROC(*)(HMODULE, LPCSTR))getprocaddressaddress)(dllhandle, funcnamerva);
+
+				 // write legit function address at firstthunk ptr
+				 *firstthunkptr = (ULONGLONG)funcaddress;
+
+
+				 ogfirstthunkptr++;
+				 firstthunkptr++;
+			 }
+
+
+
+			 importptr++;
+		 }
+
+	 }
+
+
+
+	 
+
+	 //((void(*)())((char*)baseaddress + optionalheader->AddressOfEntryPoint))();
+
+	((BOOL (*)(HMODULE,DWORD,LPVOID))
+		((char*)baseaddress + optionalheader->AddressOfEntryPoint))(NULL,DLL_PROCESS_ATTACH,NULL);
+
+}
+
+
+ 
